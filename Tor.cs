d4784d5401bfdea8e4006f26397cd1bb9983fb6d
Galus <kamil.galus.pl@gmail.com>
2019-12-15 11:36:21 +0100

    Klasa Tor

---
+++ Tor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Przeslo
+{
+    public class Tor
+    {
+        private char[] torWyscigowyDlaX = new char[20];
+        private char[] torWyscigowyDlaO = new char[20];
+        
+        public Tor()
+        {
+            for (int i = 0; i < torWyscigowyDlaX.Length; i++)
+            {
+                torWyscigowyDlaX[i] = '_';
+                torWyscigowyDlaO[i] = '_';            
+            }
+
+
+
+        }
+
+        public void WyswietlTory()
+        {
+            for (int i = 0; i < torWyscigowyDlaO.Length; i++)
+            {
+                Console.Write(torWyscigowyDlaO[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < torWyscigowyDlaX.Length; i++)
+            {
+                Console.Write(torWyscigowyDlaX[i]);
+            }
+
+        }
+
+       
+
+        public char[] ZwrocTabliceO()
+        {
+            return torWyscigowyDlaO;
+        }
+        public char[] ZwrocTabliceX()
+        {
+            return torWyscigowyDlaX;
+        }
+
+
+
+
+
+    }
+}
