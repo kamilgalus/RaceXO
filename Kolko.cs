8e506b89c22d45e0ec2bb3d6ff70fb804c1c0c12
Galus <kamil.galus.pl@gmail.com>
2019-12-15 11:35:06 +0100

    Klasa dziedziczaca po Intefacae

---
+++ Kolko.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Przeslo
+{
+    public class Kolo : IKsztalt
+    {
+        private char znakPionka = 'O';
+        private int pozycjaPionkaO;
+        private int pozycjaStartowaO;
+
+        public Kolo()
+        {
+            pozycjaStartowaO = 0;
+            pozycjaPionkaO = pozycjaStartowaO;
+        }
+
+        public Kolo(int pozycjaStartowaO)
+        {
+            this.pozycjaStartowaO = pozycjaStartowaO;
+            pozycjaPionkaO = pozycjaStartowaO;
+        }
+
+        public void UstawNaStart(char[] tor)
+        {
+            tor[pozycjaStartowaO] = znakPionka;
+        }
+
+        public void Przesun(int iloscMiejsc, char[] tor)
+        {
+            tor[pozycjaPionkaO] = '_';
+            tor[pozycjaPionkaO + iloscMiejsc] = znakPionka;
+            pozycjaPionkaO = pozycjaPionkaO + iloscMiejsc;
+        }
+    }
+}
