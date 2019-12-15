024f80fcac206d30595c94d4bf4ffac17e5ceb52
Galus <kamil.galus.pl@gmail.com>
2019-12-15 11:35:29 +0100

    Klasa dziedziczaca po interface

---
+++ Krzyz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Przeslo
+{
+    public class Krzyz : IKsztalt
+    {
+        private char znakPionka = 'X';
+        private int pozycjaPionkaX;
+        private int pozycjaStartowaX;
+
+
+        public Krzyz()
+        {
+            pozycjaStartowaX = 0;
+            pozycjaPionkaX = pozycjaStartowaX;
+            
+        }
+
+        public Krzyz(int pozycjaStartowaX)
+        {
+            this.pozycjaStartowaX = pozycjaStartowaX;
+            pozycjaPionkaX = pozycjaStartowaX;
+        }
+
+
+        public void UstawNaStart(char[] tor)
+        {
+            tor[pozycjaStartowaX] = znakPionka;
+        }
+        public void Przesun(int iloscMiejsc, char[] tor)
+        {
+            tor[pozycjaPionkaX] = '_';
+            tor[pozycjaPionkaX + iloscMiejsc] = znakPionka;
+            pozycjaPionkaX = pozycjaPionkaX + iloscMiejsc;
+        }
+
+    }
+}
