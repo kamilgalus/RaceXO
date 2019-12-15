ec755d08fb9be7eb494f8b3b500ba65073fc35ac
Galus <kamil.galus.pl@gmail.com>
2019-12-15 11:34:29 +0100

    Interface

---
+++ IKsztalt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Przeslo
+{
+    public interface IKsztalt
+    {
+
+        public void UstawNaStart(char[] tor);
+        public void Przesun(int iloscMiejsc, char[] tor);
+
+
+    }
+
+}
