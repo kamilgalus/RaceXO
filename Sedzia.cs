d19212f6a0a308389eeed19a006229cf3f22abfe
Galus <kamil.galus.pl@gmail.com>
2019-12-15 11:36:07 +0100

    Klasa Sedzia

---
+++ Sedzia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Przeslo
+{
+    public class Sedzia
+    {      
+        public bool KogoKolej(int nrRundy)
+        {
+            if (nrRundy%2== 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
